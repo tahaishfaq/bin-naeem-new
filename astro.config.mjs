6bbea0e6ed4008149addb0d9d485b5def7a05dc7
tahaishfaq <tahaishfaq71@gmail.com>
2026-03-09 16:48:39 +0500

    Flatten repo structure and move Astro app to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
